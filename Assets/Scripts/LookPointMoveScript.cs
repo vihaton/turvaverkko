6614b1cfd67e1fc955a3a1a0e63fb918a67249bc
jwinter <jwinter>
2017-02-04 19:46:20 +0200

    Liikettä yms.

---
+++ Assets/Scripts/LookPointMoveScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookPointMoveScript : MonoBehaviour {
+
+    public Vector3 endPos;
+    public float lerpTime;
+
+	public void Move()
+    {
+        Vector3 startPos = transform.position;
+        Debug.Log("Moving to: (" + endPos.x + ", " + endPos.y + ", " + endPos.z + ")");
+        StartCoroutine(MoveToPoint(startPos, endPos));
+    }
+
+    IEnumerator MoveToPoint(Vector3 startPos, Vector3 endPos)
+    {
+        float deltaTime = 0;
+        while (deltaTime < lerpTime)
+        {
+            float percentage = deltaTime / lerpTime;
+
+            //make the transition smooth af
+            percentage = percentage * percentage * percentage * (percentage * (6f * percentage - 15f) + 10f);
+
+
+            transform.position = Vector3.Lerp(startPos, endPos, percentage);
+
+            deltaTime += Time.deltaTime;
+
+            yield return null;
+        }
+    }
+}
