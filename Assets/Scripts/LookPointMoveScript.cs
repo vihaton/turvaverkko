b23285955313031c20b34d98a619bfb1be0499f4
jwinter <jwinter>
2017-02-07 01:02:06 +0200

    Stats screen aluilleen

--- Assets/Scripts/LookPointMoveScript.cs
+++ Assets/Scripts/LookPointMoveScript.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class LookPointMoveScript : MonoBehaviour {
 
-    public Vector3 endPos;
     public float lerpTime;
 
-	public void Move()
+	public void Move(GameObject goal)
     {
         Vector3 startPos = transform.position;
+        Vector3 endPos = goal.transform.position;
+        Quaternion startRot = transform.rotation;
+        Quaternion endRot = goal.transform.rotation;
+
         Debug.Log("Moving to: (" + endPos.x + ", " + endPos.y + ", " + endPos.z + ")");
-        StartCoroutine(MoveToPoint(startPos, endPos));
+
+        StartCoroutine(MoveAndRotate(startPos, endPos, startRot, endRot));
     }
 
-    IEnumerator MoveToPoint(Vector3 startPos, Vector3 endPos)
+    IEnumerator MoveAndRotate(Vector3 startPos, Vector3 endPos, Quaternion startRot, Quaternion endRot)
     {
         float deltaTime = 0;
         while (deltaTime < lerpTime)
@@ -24,8 +28,8 @@ public class LookPointMoveScript : MonoBehaviour {
             //make the transition smooth af
             percentage = percentage * percentage * percentage * (percentage * (6f * percentage - 15f) + 10f);
 
-
             transform.position = Vector3.Lerp(startPos, endPos, percentage);
+            transform.rotation = Quaternion.Lerp(startRot, endRot, percentage);
 
             deltaTime += Time.deltaTime;
 
---
+++ Assets/Scripts/ToggleStatsScreenScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleStatsScreenScript : MonoBehaviour {
+
+    public GameObject StatsScreenLookPoint;
+    public GameObject SafetyNetLookPoint;
+
+    private bool inStatsScreen = false;
+
+	public void StatsScreenToggle()
+    {
+        GameObject temp = SafetyNetLookPoint;
+
+        if (!inStatsScreen)
+        {
+            temp = StatsScreenLookPoint;
+        }
+
+        inStatsScreen = !inStatsScreen;
+
+        FindObjectOfType<LookPointMoveScript>().Move(temp);
+    }
+}
